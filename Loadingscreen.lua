ad4bdb4211b13d89a7b980ff350db15a1dcc3429
MonzzBlox <160906868+MonzzBlox@users.noreply.github.com>
2025-01-17 03:11:14 +0700

    Update Loadingscreen.lua

--- Loadingscreen.lua
+++ Loadingscreen.lua
@@ -1,218 +1,371 @@
-if LoadingScreen == true then
-    LoadingScreen = true
-LoadingText1 = "Loading Library"
-LoadingText2 = "Updating Library"
-LoadingText3 = "Getting Script"
-LoadingText4 = "Executing Script"
-LoadingText5 = "Error: Execution Failed, re-trying!"
+-- LoadingScreen3D.lua --
+-- Written by Kip Turner, copyright ROBLOX 2016 --
 
-local ScriptTitle = "LightMonzz: Script Loading"
+local GUI_DISTANCE_FROM_CAMERA = 6
+local VERTICAL_SCREEN_PERCENT = 1/3
+local HORIZONTAL_SCREEN_PERCENT = 1/3
+local SECOND_TO_FADE = 2.5
+local ROTATIONS_PER_SECOND = 0.5
+local TEXT_SCROLL_SPEED = 25
 
-if LoadingScreen ~= true then
-    local Loading = Instance.new("ScreenGui")
-    local MainFrame = Instance.new("Frame")
-    local Title = Instance.new("TextLabel")
-    local Dots = Instance.new("Frame")
-    local Dot1 = Instance.new("Frame")
-    local UICorner = Instance.new("UICorner")
-    local InsideDot = Instance.new("Frame")
-    local UICorner_2 = Instance.new("UICorner")
-    local Dot2 = Instance.new("Frame")
-    local UICorner_3 = Instance.new("UICorner")
-    local InsideDot_2 = Instance.new("Frame")
-    local UICorner_4 = Instance.new("UICorner")
-    local Dot3 = Instance.new("Frame")
-    local UICorner_5 = Instance.new("UICorner")
-    local InsideDot_3 = Instance.new("Frame")
-    local UICorner_6 = Instance.new("UICorner")
-    local Bar = Instance.new("Frame")
-    local Bar2 = Instance.new("Frame")
-    local UICorner_7 = Instance.new("UICorner")
-    local UICorner_8 = Instance.new("UICorner")
-    local Percentage = Instance.new("TextLabel")
-    local Tip = Instance.new("TextLabel")
-    local EndSequence = Instance.new("Frame")
-    Loading.Name = "LightMonzz"
-    Loading.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-    Loading.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-    Loading.DisplayOrder = 999999999
-    MainFrame.Name = "MainFrame"
-    MainFrame.Parent = Loading
-    MainFrame.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
-    MainFrame.BorderSizePixel = 0
-    MainFrame.Position = UDim2.new(-0.125662372, 0, -0.125935167, 0)
-    MainFrame.Size = UDim2.new(1.25, 0, 1.25, 0)
-    Title.Name = "Title"
-    Title.Parent = MainFrame
-    Title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    Title.BackgroundTransparency = 1.000
-    Title.Position = UDim2.new(0.183329239, 0, 0.246382296, 0)
-    Title.Size = UDim2.new(0.632528603, 0, 0.232815996, 0)
-    Title.Font = Enum.Font.FredokaOne
-    Title.Text = ScriptTitle
-    Title.TextColor3 = Color3.fromRGB(255, 255, 255)
-    Title.TextScaled = true
-    Title.TextSize = 14.000
-    Title.TextWrapped = true
-    Dots.Name = "Dots"
-    Dots.Parent = MainFrame
-    Dots.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    Dots.BackgroundTransparency = 1.000
-    Dots.Position = UDim2.new(0.470970035, 0, 0.818612278, 0)
-    Dots.Size = UDim2.new(0.0571707934, 0, 0.0360675976, 0)
-    Dot1.Name = "Dot1"
-    Dot1.Parent = Dots
-    Dot1.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-    Dot1.Position = UDim2.new(0.0611661971, 0, 0.234447539, 0)
-    Dot1.Size = UDim2.new(0.159291148, 0, 0.51158762, 0)
-    UICorner.CornerRadius = UDim.new(0.5, 0)
-    UICorner.Parent = Dot1
-    InsideDot.Name = "InsideDot"
-    InsideDot.Parent = Dot1
-    InsideDot.AnchorPoint = Vector2.new(0.5, 0.5)
-    InsideDot.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    InsideDot.Position = UDim2.new(0.5, 0, 0.5, 0)
-    UICorner_2.CornerRadius = UDim.new(0.5, 0)
-    UICorner_2.Parent = InsideDot
-    Dot2.Name = "Dot2"
-    Dot2.Parent = Dots
-    Dot2.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-    Dot2.Position = UDim2.new(0.419989735, 0, 0.234447539, 0)
-    Dot2.Size = UDim2.new(0.159291148, 0, 0.51158762, 0)
-    UICorner_3.CornerRadius = UDim.new(0.5, 0)
-    UICorner_3.Parent = Dot2
-    InsideDot_2.Name = "InsideDot"
-    InsideDot_2.Parent = Dot2
-    InsideDot_2.AnchorPoint = Vector2.new(0.5, 0.5)
-    InsideDot_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    InsideDot_2.Position = UDim2.new(0.5, 0, 0.5, 0)
-    UICorner_4.CornerRadius = UDim.new(0.5, 0)
-    UICorner_4.Parent = InsideDot_2
-    Dot3.Name = "Dot3"
-    Dot3.Parent = Dots
-    Dot3.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-    Dot3.Position = UDim2.new(0.778813243, 0, 0.234447539, 0)
-    Dot3.Size = UDim2.new(0.159291148, 0, 0.51158762, 0)
-    UICorner_5.CornerRadius = UDim.new(0.5, 0)
-    UICorner_5.Parent = Dot3
-    InsideDot_3.Name = "InsideDot"
-    InsideDot_3.Parent = Dot3
-    InsideDot_3.AnchorPoint = Vector2.new(0.5, 0.5)
-    InsideDot_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    InsideDot_3.Position = UDim2.new(0.5, 0, 0.5, 0)
-    UICorner_6.CornerRadius = UDim.new(0.5, 0)
-    UICorner_6.Parent = InsideDot_3
-    Bar.Name = "Bar"
-    Bar.Parent = MainFrame
-    Bar.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-    Bar.BorderSizePixel = 0
-    Bar.Position = UDim2.new(0.219547689, 0, 0.764966607, 0)
-    Bar.Size = UDim2.new(0.560904443, 0, 0.0365853645, 0)
-    Bar2.Name = "Bar2"
-    Bar2.Parent = Bar
-    Bar2.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
-    Bar2.BorderSizePixel = 0
-    Bar2.Size = UDim2.new(0, 0, 1, 0)
-    UICorner_7.CornerRadius = UDim.new(0.5, 0)
-    UICorner_7.Parent = Bar2
-    UICorner_8.CornerRadius = UDim.new(0.5, 0)
-    UICorner_8.Parent = Bar
-    Percentage.Name = "Percentage"
-    Percentage.Parent = Bar
-    Percentage.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    Percentage.BackgroundTransparency = 1.000
-    Percentage.Position = UDim2.new(0.280816972, 0, 0.0909090936, 0)
-    Percentage.Size = UDim2.new(0.438366145, 0, 0.787879348, 0)
-    Percentage.Font = Enum.Font.FredokaOne
-    Percentage.Text = "0%"
-    Percentage.TextColor3 = Color3.fromRGB(255, 255, 255)
-    Percentage.TextScaled = true
-    Percentage.TextSize = 14.000
-    Percentage.TextWrapped = true
-    Tip.Name = "Tip"
-    Tip.Parent = MainFrame
-    Tip.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    Tip.BackgroundTransparency = 1.000
-    Tip.Position = UDim2.new(0.280816913, 0, 0.717294872, 0)
-    Tip.Size = UDim2.new(0.438366145, 0, 0.0299334861, 0)
-    Tip.Font = Enum.Font.SourceSansItalic
-    Tip.Text = ""
-    Tip.TextColor3 = Color3.fromRGB(138, 138, 138)
-    Tip.TextScaled = true
-    Tip.TextSize = 14.000
-    Tip.TextWrapped = true
-    EndSequence.Name = "EndSequence"
-    EndSequence.Parent = Loading
-    EndSequence.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
-    EndSequence.BorderSizePixel = 0
-    EndSequence.Position = UDim2.new(1, 0, 0, 0)
-    EndSequence.Size = UDim2.new(1, 0, 1, 0)
-    local function NGZQAMK_fake_script()
-        local script = Instance.new("LocalScript", Dots)
-        while true do
-            wait(0.25)
-            script.Parent.Dot1.InsideDot:TweenSize(UDim2.new(1, 0, 1, 0), "In", "Sine", 0.25, true)
-            wait(0.25)
-            script.Parent.Dot2.InsideDot:TweenSize(UDim2.new(1, 0, 1, 0), "In", "Sine", 0.25, true)
-            wait(0.25)
-            script.Parent.Dot3.InsideDot:TweenSize(UDim2.new(1, 0, 1, 0), "In", "Sine", 0.25, true)
-            wait(0.25)
-            script.Parent.Dot1.InsideDot:TweenSize(UDim2.new(0, 0, 0, 0), "In", "Sine", 0.25, true)
-            wait(0.25)
-            script.Parent.Dot2.InsideDot:TweenSize(UDim2.new(0, 0, 0, 0), "In", "Sine", 0.25, true)
-            wait(0.25)
-            script.Parent.Dot3.InsideDot:TweenSize(UDim2.new(0, 0, 0, 0), "In", "Sine", 0.25, true)
-        end
-    end
+local CoreGui = game:GetService('CoreGui')
+local RunService = game:GetService('RunService')
+local MarketPlaceService = game:GetService('MarketplaceService')
+local UserInputService = game:GetService('UserInputService')
+local ReplicatedFirst = game:GetService('ReplicatedFirst')
+local GuiService = game:GetService('GuiService')
 
-    coroutine.wrap(NGZQAMK_fake_script)()
-    local function NDID_fake_script()
-        local script = Instance.new("LocalScript", MainFrame)
-        local bar = script.Parent.Bar
-        local insidebar = bar.Bar2
-        local percentage = bar.Percentage
-        wait(5)
-        insidebar:TweenSize(UDim2.new(1, 0, 1, 0), "In", "Linear", 200, true)
-    end
-    coroutine.wrap(NDID_fake_script)()
-    local function OVSBNB_fake_script()
-        local script = Instance.new("LocalScript", Percentage)
-        wait(5)
-        for i = 1, 100 do
-            script.Parent.Text = i .. "%"
-            wait(2)
-        end
-    end
-    coroutine.wrap(OVSBNB_fake_script)()
-    local function SNUMK_fake_script()
-        local script = Instance.new("LocalScript", Tip)
-        local tip = script.Parent
-        while true do
-            wait(5)
-            tip.Text = LoadingText1
-            wait(5)
-            tip.Text = LoadingText2
-            wait(5)
-            tip.Text = LoadingText3
-            wait(5)
-            tip.Text = LoadingText4
-            wait(5)
-            tip.Text = LoadingText5
-        end
-    end
-    coroutine.wrap(SNUMK_fake_script)()
-    local function LSNGUH_fake_script()
-        local script = Instance.new("LocalScript", Loading)
-        game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)
-        game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
-        game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
-        game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.EmotesMenu, false)
-    end
-    coroutine.wrap(LSNGUH_fake_script)()
+local RobloxGui = CoreGui:WaitForChild("RobloxGui")
+local Util = require(RobloxGui.Modules.Settings.Utility)
+
+local function FadeElements(element, newValue, duration)
+	duration = duration or 0.5
+	if element == nil then return end
+	if element:IsA('ImageLabel') or element:IsA('ImageButton') then
+		Util:TweenProperty(element, 'ImageTransparency', element.ImageTransparency, newValue, duration, Util:GetEaseInOutQuad())
+	end
+	if element:IsA('GuiObject') then
+		Util:TweenProperty(element, 'BackgroundTransparency', element.BackgroundTransparency, newValue, duration, Util:GetEaseInOutQuad())
+	end
+	if element:IsA('TextLabel') or element:IsA('TextBox') or element:IsA('TextButton') then
+		Util:TweenProperty(element, 'TextTransparency', element.TextTransparency, newValue, duration, Util:GetEaseInOutQuad())
+	end
+	for _, child in pairs(element:GetChildren()) do
+		FadeElements(child, newValue, duration)
+	end
 end
 
-game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)
-game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
-game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
-game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.EmotesMenu, false)
+
+local GameInfoProvider = {}
+do
+	local LoadingFinishedSignal = Instance.new('BindableEvent')
+	GameInfoProvider.Finished = false
+	GameInfoProvider.GameAssetInfo = nil
+	GameInfoProvider.LoadingFinishedEvent = LoadingFinishedSignal.Event
+	
+	function GameInfoProvider:GetGameName()
+		if self.GameAssetInfo ~= nil then
+			return self.GameAssetInfo.Name
+		else
+			return ''
+		end
+	end
+
+	function GameInfoProvider:GetCreatorName()
+		if self.GameAssetInfo ~= nil then
+			return self.GameAssetInfo.Creator.Name
+		else
+			return ''
+		end
+	end
+
+	function GameInfoProvider:IsReady()
+		return self.Finished
+	end
+
+	function GameInfoProvider:LoadAssetsAsync()
+		spawn(function()
+			while game.PlaceId <= 0 do
+				wait()
+			end
+
+			-- load game asset info
+			local success, result = pcall(function()
+				self.GameAssetInfo = MarketPlaceService:GetProductInfo(game.PlaceId)
+			end)
+			if not success then
+				print("LoadingScript->GameInfoProvider:LoadAssets:", result)
+			end
+			self.Finished = true
+			LoadingFinishedSignal:Fire()
+		end)
+	end
 end
+
+
+
+
+local LoadingScreen = {}
+
+
+local surfaceGuiAdorn = Util:Create'Part'
+{
+	Name = "LoadingGui";
+	Transparency = 1;
+	CanCollide = false;
+	Anchored = true;
+	Archivable = false;
+	FormFactor = Enum.FormFactor.Custom;
+	RobloxLocked = true;
+	Parent = RobloxGui;
+}
+
+local loadingSurfaceGui = Util:Create'SurfaceGui'
+{
+	Name = "LoadingSurfaceGui";
+	Adornee = surfaceGuiAdorn;
+	ToolPunchThroughDistance = 1000;
+	CanvasSize = Vector2.new(500, 500);
+	Archivable = false;
+	Parent = CoreGui;
+}
+
+local backgroundImage = Util:Create'ImageLabel'
+{
+	Name = 'LoadingBackground';
+	Size = UDim2.new(1,0,1,0);
+	Image = 'rbxasset://textures/ui/LoadingScreen/BackgroundLight.png';
+	ScaleType = Enum.ScaleType.Slice;
+	SliceCenter = Rect.new(70,70,110,110);
+	BackgroundTransparency = 1;
+	Parent = loadingSurfaceGui;
+}
+
+local spinnerRotation = 0
+local spinnerImage = Util:Create'ImageLabel'
+{
+	Name = 'Spinner';
+	Size = UDim2.new(0.25,0,0.25,0);
+	Position = UDim2.new(0.5 - (0.25/2), 0, 0.45 - (0.25/2), 0);
+	Image = 'rbxasset://textures/ui/LoadingScreen/LoadingSpinner.png';
+	BackgroundTransparency = 1;
+	Parent = backgroundImage;
+}
+
+
+local loadingText = Util:Create'TextLabel'
+{
+	Name = 'LoadingText';
+	Text = 'Loading...';
+	BackgroundTransparency = 1;
+	Font = Enum.Font.SourceSans;
+	FontSize = Enum.FontSize.Size60;
+	Position = UDim2.new(0.5,0,0.2,0);
+	Parent = backgroundImage;
+}
+
+local gameNameText = Util:Create'TextLabel'
+{
+	Name = 'GameNameText';
+	Text = '';
+	BackgroundTransparency = 1;
+	Font = Enum.Font.SourceSans;
+	FontSize = Enum.FontSize.Size60;
+	Size = UDim2.new(0.9, 0, 0.1, 0);
+	Position = UDim2.new(0.05,0,0.65,0);
+	ClipsDescendants = true;
+	Parent = backgroundImage;
+}
+
+
+local creatorTextContainer = Util:Create'Frame'
+{
+	Name = 'CreatorTextContainer';
+	Size = UDim2.new(0.9, 0, 0.1, 0);
+	Position = UDim2.new(0.05,0,0.77,0);
+	BackgroundTransparency = 1;
+	ClipsDescendants = true;
+	Parent = backgroundImage;
+}
+
+local creatorTextPosition = 0
+local creatorText = Util:Create'TextLabel'
+{
+	Name = 'CreatorText';
+	Text = '';
+	BackgroundTransparency = 1;
+	Font = Enum.Font.SourceSans;
+	FontSize = Enum.FontSize.Size42;
+	Size = UDim2.new(1, 0, 1, 0);
+	Parent = creatorTextContainer;
+}
+
+
+local function ScreenDimsAtDepth(depth)
+	local camera = workspace.CurrentCamera
+	if camera then
+		local aspectRatio = camera.ViewportSize.x / camera.ViewportSize.y
+		local studHeight = 2 * depth * math.tan(math.rad(camera.FieldOfView/2))
+		local studWidth = studHeight * aspectRatio
+
+		return Vector2.new(studWidth, studHeight)
+	end
+	return Vector2.new(0,0)
+end
+
+local CleanedUp = false
+local freeze = true
+delay(2.5, function()
+	freeze = false
+end)
+local function UpdateLayout(delta)
+	local screenDims = ScreenDimsAtDepth(GUI_DISTANCE_FROM_CAMERA)
+
+	surfaceGuiAdorn.Size = Vector3.new(screenDims.x * HORIZONTAL_SCREEN_PERCENT, screenDims.y * VERTICAL_SCREEN_PERCENT, 1)
+	local camera = workspace.CurrentCamera
+	if camera then
+		surfaceGuiAdorn.Parent = camera
+	end
+
+
+	if creatorText.TextBounds.X < creatorTextContainer.AbsoluteSize.X then
+		creatorText.Position = UDim2.new(0, 0, 0, 0)
+		creatorText.Size = UDim2.new(1, 0, 1, 0)
+	elseif delta ~= nil then
+		creatorText.Size = UDim2.new(0, creatorText.TextBounds.X, 1, 0)
+		if not freeze then
+			local newX = (creatorTextPosition - delta * TEXT_SCROLL_SPEED)
+			if newX + creatorText.AbsoluteSize.X < creatorTextContainer.AbsoluteSize.X then
+				freeze = true
+				spawn(function()
+					Util:TweenProperty(creatorText, 'TextTransparency', creatorText.TextTransparency, 1, 1, Util:GetEaseInOutQuad())
+					wait(1.5)
+					if CleanedUp then return end
+					creatorTextPosition = 0
+					Util:TweenProperty(creatorText, 'TextTransparency', creatorText.TextTransparency, 0, 1, Util:GetEaseInOutQuad())
+					wait(1.5)
+					freeze = false
+				end)
+			else
+				creatorTextPosition = newX
+			end
+		end
+		creatorText.Position = UDim2.new(0, creatorTextPosition, 0, 0)
+	end
+
+	if not gameNameText.TextFits then
+		gameNameText.Size = UDim2.new(0.9, 0, 0.3, 0)
+		gameNameText.Position = UDim2.new(0.05,0,0.5,0)
+		gameNameText.TextScaled = true
+		gameNameText.TextWrapped = true
+
+		spinnerImage.Position = UDim2.new(0.5 - (0.25/2), 0, 0.225, 0)
+		loadingText.Position = UDim2.new(0.5,0,0.15,0)
+	end
+
+end
+
+local CameraChangedConn = nil
+local WorkspaceChangedConn = nil
+
+local function CleanUp()
+	if CleanedUp then return end
+	CleanedUp = true
+
+	FadeElements(loadingSurfaceGui, 1, SECOND_TO_FADE)
+	wait(SECOND_TO_FADE)
+
+	RunService:UnbindFromRenderStep("LoadingGui3D")
+	surfaceGuiAdorn.Parent = nil
+	if CameraChangedConn then
+		CameraChangedConn:disconnect()
+		CameraChangedConn = nil
+	end
+	if WorkspaceChangedConn then
+		WorkspaceChangedConn:disconnect()
+		WorkspaceChangedConn = nil
+	end
+end
+
+local function OnGameInfoLoaded()
+	local creatorName = GameInfoProvider:GetCreatorName()
+	if creatorName and creatorName ~= '' then
+		creatorName = string.format("By %s", tostring(creatorName))
+	end
+	gameNameText.Text = GameInfoProvider:GetGameName()
+	creatorText.Text = creatorName
+end
+
+local function OnReplicatingFinishedAsync()
+	local function OnGameLoaded()
+		CleanUp()
+	end
+
+	if game:IsLoaded() then
+		OnGameLoaded()
+	else
+		game.Loaded:wait()
+		OnGameLoaded()
+	end
+end
+
+local function OnDefaultLoadingGuiRemoved()
+	CleanUp()
+end
+
+
+local function UpdateSurfaceGuiPosition()
+	local camera = workspace.CurrentCamera
+	if camera then
+		local cameraCFrame = camera.CFrame
+		local cameraLook = cameraCFrame.lookVector
+
+		surfaceGuiAdorn.CFrame = (cameraCFrame * CFrame.Angles(0,math.pi,0)) + cameraLook * GUI_DISTANCE_FROM_CAMERA
+	end
+end
+
+do
+	local lastUpdate = tick()
+	RunService:BindToRenderStep("LoadingGui3D", Enum.RenderPriority.Last.Value, function()
+		local now = tick()
+		local delta = now - lastUpdate
+
+		UpdateSurfaceGuiPosition()
+		UpdateLayout(delta)
+
+		local rotation = delta * ROTATIONS_PER_SECOND * 360
+		spinnerRotation = spinnerRotation + rotation
+		spinnerImage.Rotation = spinnerRotation
+
+		lastUpdate = now
+	end)
+	UpdateSurfaceGuiPosition()
+	UpdateLayout()
+
+	local function connectCameraEvent()
+		if workspace.CurrentCamera then
+			if CameraChangedConn then
+				CameraChangedConn:disconnect()
+			end
+			CameraChangedConn = workspace.CurrentCamera:GetPropertyChangedSignal("CFrame"):connect(function()
+				UpdateSurfaceGuiPosition()
+			end)
+		end
+	end
+	WorkspaceChangedConn = workspace.Changed:connect(function(prop)
+		if prop == 'CurrentCamera' then
+			connectCameraEvent()
+		end
+	end)
+
+	connectCameraEvent()
+end
+
+GameInfoProvider:LoadAssetsAsync()
+if GameInfoProvider:IsReady() then
+	OnGameInfoLoaded()
+end
+GameInfoProvider.LoadingFinishedEvent:connect(OnGameInfoLoaded)
+
+
+if ReplicatedFirst:IsFinishedReplicating() then
+	spawn(OnReplicatingFinishedAsync)
+else
+	ReplicatedFirst.FinishedReplicating:connect(OnReplicatingFinishedAsync)
+end
+
+if ReplicatedFirst:IsDefaultLoadingGuiRemoved() then
+	OnDefaultLoadingGuiRemoved()
+else
+	ReplicatedFirst.RemoveDefaultLoadingGuiSignal:connect(OnDefaultLoadingGuiRemoved)
+end
+
+GuiService.ErrorMessageChanged:connect(function()
+	-- TODO
+end)
+
+GuiService.UiMessageChanged:connect(function(type, newMessage)
+	-- TODO
+end)
+
+return LoadingScreen
